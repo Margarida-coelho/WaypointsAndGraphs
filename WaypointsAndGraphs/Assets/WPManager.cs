4e2d197cd864e484aa3a881b890c6a654b7db15a
Margarida-coelho <162127564+Margarida-coelho@users.noreply.github.com>
2024-10-28 19:28:30 +0000

    Using A* with Waypoints Part 1

---
+++ WaypointsAndGraphs/Assets/WPManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Link
+{
+    public enum Direction
+    {
+        UNI,
+        BI
+    }
+
+    public GameObject node1;
+    public GameObject node2;
+    public Direction direction;
+}
+
+public class WPManager : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    public Link[] links;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
